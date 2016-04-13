0fd76dcae8f3eb17eef5b396aaa0651942cdc298
FelixN7 <FelixN7>
2016-04-13 11:11:46 +0200

    debut de script d homologation

--- RasPi/crabot/homologation.py
+++ RasPi/crabot/homologation.py
@@ -9,14 +9,19 @@ functions.reset_pic()
 functions.init_ax(ser)
 
 # Attente du start
-#answer = functions.get_ans(ser)
-#while answer != "$STRT;":
-#    answer = functions.get_ans(ser)
+print "waiting for start"
+answer = functions.get_ans(ser)
+while answer != "$STRT;":
+    answer = functions.get_ans(ser)
+    print answer
 
-# Demande de l'équipe
+# Demande de l'equipe
 team = functions.ask_team(ser)
 
 functions.open_bras(ser)
+functions.funny_action(ser)
+
+functions.move_pos(ser,0.85,0)
 
 answer = functions.get_ans(ser)
 while answer != "$DONE;":
@@ -27,5 +32,35 @@ functions.move_pos(ser,0.4,0)
 answer = functions.get_ans(ser)
 while answer != "$DONE;":
     answer = functions.get_ans(ser)
+
+functions.rotate(ser,team*1.5708)
+
+answer = functions.get_ans(ser)
+while answer != "$DONE;":
+    answer = functions.get_ans(ser)
+
+functions.move_pos(ser,0.4,team*0.45)
+
+answer = functions.get_ans(ser)
+while answer != "$DONE;":
+    answer = functions.get_ans(ser)
+
+functions.rotate(ser,0)
+
+answer = functions.get_ans(ser)
+while answer != "$DONE;":
+    answer = functions.get_ans(ser)
+
+while True:
+   functions.move_pos(ser,1.1,team*0.45)
+  
+   answer = functions.get_ans(ser)
+   while answer != "$DONE;":
+      answer = functions.get_ans(ser)
+
+   functions.move_pos(ser,0.2,team*0.45)
+   answer = functions.get_ans(ser)
+   while answer != "$DONE;":
+       answer = functions.get_ans(ser)
 
 ser.close()
