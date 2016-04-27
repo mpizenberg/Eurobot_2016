f7781c9e2abe70a82f683eab4b492e1af8394034
HA-pepino <pepino.francois@gmail.com>
2016-04-28 00:42:38 +0200

    Modification des Pins pour les pompes.. (je sais pas qui a câblé ça, mais ze vais l'engueuler...)

--- Motopompe.X/main.h
+++ Motopompe.X/main.h
@@ -6,8 +6,8 @@
 #define LED LATCbits.LATC6
 #define TRIS_LED TRISCbits.TRISC6
 
-#define POMPE_HAUTE PORTAbits.RA6
-#define POMPE_BASSE PORTAbits.RA2
+#define POMPE_HAUTE PORTAbits.RA7
+#define POMPE_BASSE PORTAbits.RA6
 
 
 
