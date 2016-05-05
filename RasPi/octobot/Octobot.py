2e5b1386dfcd01992f93e24b09a9a54abfd7e3e7
Thibaut Boissin <thibaut@localhost.localdomain>
2016-05-05 04:43:45 +0200

    copie arret urgence + fichier

--- RasPi/octobot/Octobot.py
+++ RasPi/octobot/Octobot.py
@@ -3,13 +3,26 @@
 import serial
 import functions
 import functions_macro
+from UrgenceReleveException import UrgenceReleveException
+
 
 ser = serial.Serial("/dev/ttyAMA0",57600)
 
 functions.reset_pic()
 functions.init_ax(ser)
-while 1:
+#ouverture du fichier
+continuer = True
+while continuer:
 
+    #lecture du fichier pour savoir si l'on sort de la boucle
+    mon_fichier = open("~/Eurobot_2016/RasPi/crabot/quitter.txt","r")
+    if mon_fichier.read()=="exit\n":
+        mon_fichier.close()
+        continuer = False
+        break
+    mon_fichier.close()
+
+    try:
 	# Attente du start
 	print "waiting for start"
 	answer = functions.get_ans(ser)
@@ -27,7 +40,9 @@ while 1:
 	functions_macro.Prise_Colonne_De_Sable(ser,team,1.4,0.6)
 	functions_macro.Prise_Colonne_De_Sable(ser,team,1.4,0.6)
 	functions_macro.Prise_Colonne_De_Sable(ser,team,1.4,0.6)
-	
+    
+    except UrgenceReleveException as e:
+        print e.value
 
 ser.close()
 
