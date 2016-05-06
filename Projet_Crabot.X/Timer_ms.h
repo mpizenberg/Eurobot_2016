3907b3720d037936a41ba46381c3dad26be6283d
HA-pepino <pepino.francois@gmail.com>
2016-05-06 12:35:57 +0200

    enlève de 2 maxtimes non utilisés pour gagner qq us dans l'IT, renommage du delay watchdog attention, ce commit ne renomme pas la fonction Raz_Delay_10() => Raz_Delay_WatchDone() dans communication

--- Projet_Crabot.X/Timer_ms.h
+++ Projet_Crabot.X/Timer_ms.h
@@ -7,12 +7,15 @@ extern volatile char Delay_90_Over;
 
 void Timer_ms_Init(void);
 
-unsigned int Get_Maxtime_Seq_AX12(void);
-void Set_Maxtime_Seq_AX12(unsigned int val);
+//unsigned int Get_Maxtime_Seq_AX12(void);
+//void Set_Maxtime_Seq_AX12(unsigned int val);
+//
+//void Set_Maxtime(unsigned int val);
+//char Get_Maxtime(void);
 
-void Set_Maxtime(unsigned int val);
-char Get_Maxtime(void);
-void Raz_Delay_10(void);
+//ms
+#define MAX_DELAY_WATCHDONE  5000   
+void Raz_Delay_WatchDone(void);
 
 
 #endif
